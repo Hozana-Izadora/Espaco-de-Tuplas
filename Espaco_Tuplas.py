89de5bdd3b5429cb6206306f83da66e5989b8016
hozana-izadora <izadoraferreir@gmail.com>
2022-12-06 23:36:59 -0300

    create read delete

--- Espaco_Tuplas.py
+++ Espaco_Tuplas.py
@@ -23,50 +23,71 @@ def criaNuvem(nomeNuvem):
 def criaHost(nomeHost): 
     print(f"Host: {nomeHost} criada")
     tse.out((nomeHost))
-
+    tsHosts = []
     try:
         hosts = tse.inp(("HOSTS", object))
         tsHosts = list(hosts[1])
         tsHosts.append(nomeHost)
         tse.out(("HOSTS", tuple(tsHosts)))
     except:
-        tsHosts = (nomeHost)
+        tsHosts = [nomeHost]
         tse.out(("HOSTS", tuple(tsHosts)))    
 #end criaHost
 
 def criaVM(nomeVM): 
     print(f"VM: {nomeVM} criada")
     tse.out((nomeVM))
-
+    tsVms = []
     try:
         vms = tse.inp(("VMS", object))
         tsVms = list(vms[1])
         tsVms.append(nomeVM)
         tse.out(("VMS", tuple(tsVms)))
     except:
-        tsVms = (nomeVM)
+        tsVms = [nomeVM]
         tse.out(("VMS", tuple(tsVms)))
 #end criaVM
 
 def criaProcesso(nomeProcesso):
     print(f"Processo: {nomeProcesso} criada")
     tse.out((nomeProcesso))
-
+    tsProcessos = []
     try:
-        processos = tse.inp(("VMS", object))
+        processos = tse.inp(("PROCESSOS", object))
         tsProcessos = list(processos[1])
         tsProcessos.append(nomeProcesso)
         tse.out(("PROCESSOS", tuple(tsProcessos)))
     except:
-        tsProcessos = (nomeProcesso)
+        tsProcessos = [nomeProcesso]
         tse.out(("PROCESSOS", tuple(tsProcessos)))
 
-def getTuple(nome):
-    tse.timeout(1)
-    tupla = tse.rdp((nome))
-    print(tupla)
-#end getNuvem
 
+def deleteItem(tipo_tupla,nome):
+    try:
+        tupla = tse.inp((tipo_tupla,nome))
+        return 1
+    except:
+        return 0
+def deleteTuple(tipo_tupla, nome):
+    try:
+        tupla = tse.inp((tipo_tupla,object))
+        nova_tupla = list(tupla[1])
+        nova_tupla.remove(nome)
+        tse.out((tipo_tupla,tuple(nova_tupla)))
+        return(f"Tupla {nome} excluida")
+    except:
+        return(f"Tupla {nome} não encontrada")
+
+def existe(tipo_tupla, nome):
+    try:
+        tupla = tse.rdp((tipo_tupla,nome))
+        print(f"TUPLA {tupla[1]} JÁ EXISTE")
+        return 1
+    except:
+        return 0
+        
+
+#listagem - read
 def listNuvens():
     try:
         nuvens = tse.rdp(("NUVENS",object))    
@@ -74,7 +95,25 @@ def listNuvens():
         return list(nuvens[1])
     except: 
         print(f"Tuple matching not found")
+def listHosts():
+    try:
+        hosts = tse.rdp(("HOSTS",object))    
+        print(hosts)
+        return list(hosts[1])
+    except: 
+        print(f"Tuple matching not found")
+def listVMs():
+    try:
+        vms = tse.rdp(("VMS",object))    
+        print(vms)
+        return list(vms[1])
+    except: 
+        print(f"Tuple matching not found")
+def listProcessos():
+    try:
+        processos = tse.rdp(("PROCESSOS",object))    
+        print(processos)
+        return list(processos[1])
+    except: 
+        print(f"Tuple matching not found")
 
-# criaNuvem("aws")
-# criaNuvem("google")
-# criaNuvem("drive")
--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QMessageBox
 import linsimpy
 
 import Espaco_Tuplas as ts
@@ -9,48 +10,115 @@ hosts = []
 vms = []
 processos = []
 
+
 tse = linsimpy.TupleSpaceEnvironment()
 tse.out(("NUVENS", tuple(nuvens)))
 tse.out(("HOSTS", tuple(hosts)))
 tse.out(("VMS", tuple(vms)))
 tse.out(("PROCESSOS", tuple(processos)))
 
+
+def atualizaTela():
+    lista_nuvens = ts.listNuvens()
+    lista_hosts = ts.listHosts()
+    lista_vms = ts.listVMs()
+    lista_processos = ts.listProcessos()
+    openSegundaTela(lista_nuvens,lista_hosts,lista_vms,lista_processos)
+    primeira_tela.lineEdit.clear()
+    primeira_tela.lineEdit_2.clear()
+    primeira_tela.lineEdit_3.clear()
+    primeira_tela.lineEdit_4.clear()
+
 def cadastraContainer():
     nuvem    = primeira_tela.lineEdit.text()
     host     = primeira_tela.lineEdit_2.text()
     vm       = primeira_tela.lineEdit_3.text()
     processo = primeira_tela.lineEdit_4.text()
-
-    data = [nuvem, host,vm, processo]
     
+    data = [nuvem, host,vm, processo]
     if(data[0]!= ''):
-        nomeNuvem = data[0]
-        ts.criaNuvem(nomeNuvem)
+        nomeNuvem = data[0]             
+        if(ts.existe("NUVEM",nomeNuvem) == 0):
+            ts.criaNuvem(nomeNuvem)
+        else:
+            QMessageBox.about(primeira_tela,"ALERTA", "NUVEM JÁ EXISTE")            
     if(data[1]!= ''):
         nomeHost = data[1]
-        ts.criaHost(nomeHost)
+        if(ts.existe("HOST",nomeHost) == 0):
+            ts.criaHost(nomeHost)
+        else:
+            QMessageBox.about(primeira_tela,"ALERTA", "HOST JÁ EXISTE")  
     if(data[2]!= ''):
         nomeVM = data[2]
-        ts.criaVM(nomeVM)
+        if(ts.existe("VM",nomeVM) == 0):
+            ts.criaVM(nomeVM)
+        else:
+            QMessageBox.about(primeira_tela,"ALERTA", "VM JÁ EXISTE")  
+
     if(data[3]!= ''):
         nomeProcesso = data[3]
-        ts.criaProcesso(nomeProcesso)
-    lista_nuvens = ts.listNuvens()
-    openSegundaTela(lista_nuvens)
+        if(ts.existe("PROCESSO",nomeProcesso) == 0):
+            ts.criaProcesso(nomeProcesso)
+        else:
+            QMessageBox.about(primeira_tela,"ALERTA", "PROCESSO JÁ EXISTE")  
 
-def openSegundaTela(itens):
+    atualizaTela()
+
+def openSegundaTela(nuvens,hosts,vms,processos):
     segunda_tela.show()
-    for i in itens:
-        segunda_tela.listView.addItem(i)
+    segunda_tela.listWidget.clear()
+    segunda_tela.listWidget_2.clear()
+    segunda_tela.listWidget_3.clear()
+    segunda_tela.listWidget_4.clear()
 
-    
-    
+    if(len(nuvens)>0):
+        for n in nuvens:
+            segunda_tela.listWidget.addItem(n)
+            segunda_tela.label_6.setText(str(len(nuvens)))
+    if(len(hosts)>0):            
+        for h in hosts:
+            segunda_tela.listWidget_2.addItem(h)
+            segunda_tela.label_8.setText(str(len(hosts)))
+    if(len(vms)>0):
+        for v in vms:
+            segunda_tela.listWidget_3.addItem(v)
+            segunda_tela.label_7.setText(str(len(vms)))
+    if(len(processos)>0):
+        for p in processos:
+            segunda_tela.listWidget_4.addItem(p)
+            segunda_tela.label_9.setText(str(len(processos)))
+
+def excluiItem():
+    nuvem    = primeira_tela.lineEdit.text()
+    host     = primeira_tela.lineEdit_2.text()
+    vm       = primeira_tela.lineEdit_3.text()
+    processo = primeira_tela.lineEdit_4.text() 
+    if(nuvem != ''):
+        ts.deleteItem("NUVEM",nuvem)
+        response =ts.deleteTuple("NUVENS",nuvem)        
+        QMessageBox.about(primeira_tela,"ALERTA", response) 
+        atualizaTela()     
+    if(host != ''):
+        ts.deleteItem("HOST",host)
+        response =ts.deleteTuple("HOSTS",host)        
+        QMessageBox.about(primeira_tela,"ALERTA", response) 
+        atualizaTela()     
+    if(vm != ''):
+        ts.deleteItem("VM",vm)
+        response =ts.deleteTuple("VMS",vm)        
+        QMessageBox.about(primeira_tela,"ALERTA", response) 
+        atualizaTela()     
+    if(processo != ''):
+        ts.deleteItem("PROCESSO",processo)
+        response =ts.deleteTuple("PROCESSOS",processo)        
+        QMessageBox.about(primeira_tela,"ALERTA", response) 
+        atualizaTela()     
 
 app=QtWidgets.QApplication([])
 primeira_tela=uic.loadUi("primeira_tela.ui")
 primeira_tela.show()
 segunda_tela=uic.loadUi("segunda_tela.ui")
 primeira_tela.pushButton.clicked.connect(cadastraContainer)
-# primeira_tela.pushButton.clicked.connect(openSegundaTela)
+primeira_tela.pushButton_2.clicked.connect(excluiItem)
 
 app.exec()
